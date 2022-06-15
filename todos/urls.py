38f2aaf85d60e893ae7445968d8e83dd83a41c69
nicolas asparria <nasparria@gmail.com>
2022-06-14 21:59:21 -0500

    Feature 8 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from todos.views import (
     TodoListView,
     TodoListDetailView,
+    TodoListCreateView,
+
 )
 
 urlpatterns = [
   path("", TodoListView.as_view(), name="todo_list"),
   path("<int:pk>/", TodoListDetailView.as_view(), name="todo_list_detail"),
+  path("create/", TodoListCreateView.as_view(), name="todo_list_create")
 
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,10 @@
 # from django.shortcuts import render
+from django.urls import reverse_lazy
 from todos.models import TodoList
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
@@ -18,3 +20,12 @@ class TodoListDetailView(DetailView):
     model = TodoList
     template_name = "todos/detail.html"
     context_object_name = "todo_list_detail"
+
+
+class TodoListCreateView(CreateView):
+    model = TodoList
+    template_name = "todos/create.html"
+    fields = ["name"]
+
+    def get_success_url(self):
+        return reverse_lazy("todo_list_detail", kwargs={"pk":self.object.pk})
